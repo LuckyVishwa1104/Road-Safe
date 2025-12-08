ea3e98460864b9b5b6edd93d9437f58d9541e765
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2025-12-08 23:39:07 +0530

    removed unneccesory string concatenation

--- mobileApp/lib/config.dart
+++ mobileApp/lib/config.dart
@@ -4,18 +4,18 @@ final url = 'https://the-road-safe-backend.vercel.app/';
 // for local server
 // final url = 'http://192.168.231.212:3000/';
 
-final registration = url + "registration";
+final registration = "${url}registration";
 
-final login = url + 'login';
+final login = "${url}login";
 
-final complaintDetails = url + 'complaintDetails';
+final complaintDetails = '${url}complaintDetails';
 
-final getComplaintData = url + 'getComplaintDetails';
+final getComplaintData = '${url}getComplaintDetails';
 
-final deleteComplaints = url + 'deleteComplaint';
+final deleteComplaints = '${url}deleteComplaint';
 
-final userDetail = url + 'userDetails';
+final userDetail = '${url}userDetails';
 
-final uploadPhoto = url + 'uploadPhoto';
+final uploadPhoto = '${url}uploadPhoto';
 
-final uploadUserPhoto = url + 'uploadUserPhoto';
+final uploadUserPhoto = '${url}uploadUserPhoto';
