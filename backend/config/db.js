3e21ec919d2357fac65d363505ec12d47874adc7
Lucky Vishwakarma <luckyvishwa1104@gmail.com>
2025-12-29 09:09:12 +0530

    fix(url): add .env variable for connection URL

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection('mongodb+srv://luckyvishwa1104:lucky1104@cluster0.i72lmi2.mongodb.net/UserLoginDetails?retryWrites=true&w=majority').on('open', () => {
+require('dotenv').config();
+const connection = mongoose.createConnection(process.env.MONGODB_CONN_URL).on('open', () => {
     console.log("MongoDB Connected.");
 }).on('error', () => {
     console.log("MongoDB not connected.")
